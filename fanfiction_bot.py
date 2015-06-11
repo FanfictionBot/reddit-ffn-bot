d826e5925ca4ad002276d31ef2bfd1b878cadedc
MikroMan <subic.tine@gmail.com>
2015-06-11 21:17:21 +0200

    More reformatting, function names.

--- fanfiction_bot.py
+++ fanfiction_bot.py
@@ -21,6 +21,7 @@ DONE = []
 def main():
     # DONE = pickle.load(replies)
     login()
+
     while True:
         parse_submissions()
         pause(30, 0)
@@ -37,6 +38,7 @@ def pause(minutes, seconds):
 
 
 def parse_arguments():
+    #initialize parser and add options for username and password
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--user',help='define Reddit login username')
     parser.add_argument('-p','--password', help='define Reddit login password')
@@ -47,22 +49,23 @@ def parse_arguments():
 
 
 def login():
-    user_name, user_pw = parse_arguments()
-    # Moved name and PW to local to prevent potential hack.
+    global SUBREDDIT
 
+    user_name, user_pw = parse_arguments()
     print("Logging in...")
     r.login(user_name, user_pw)
-    global SUBREDDIT
+
     print("Loading subreddit...")
     SUBREDDIT = r.get_subreddit('HPFanfiction')
     print('Loading DONE...')
-    global DONE
+
     with open('done.txt', 'r') as file:
         DONE = [str(line.rstrip('\n')) for line in file]
 
-
+ #Mark what as done? add more descriptive method name?
 def markdone(id):
     DONE.append(str(id))
+
     with open('done.txt', 'w') as file:
         for id in DONE:
             file.write(str(id) + '\n')
@@ -86,54 +89,57 @@ def parse_submissions():
 
 
 def parse_comment(comment, id):
+    footer = "\n*Graciously brought to you by me - /u/tusing's bot.*"
     REGEXPS = {'linkffn\((.*?)\)': 'ffn'}
     requested = {}
+
     contents = comment.body
     for expr in REGEXPS.keys():
         tofind = re.findall(expr, contents)
         requested[REGEXPS[expr]] = tofind
     print("FINDING: ", requested)
-    links = ffn_linkfinder(requested['ffn'])
-    found_ffn = ffn_commentmaker(links)
-    footer = "\n*Graciously brought to you by me - /u/tusing's bot.*"
+    links = ffn_link_finder(requested['ffn'])
+    found_ffn = ffn_comment_maker(links)
     if len(found_ffn) > 10:
         print('Outgoing reply to ' + id + ':\n' + found_ffn + footer)
         comment.reply(found_ffn + footer)
         pause(9, 5)  # pause for 9 minutes and 5 seconds before new comment
 
 
-def ffn_linkfinder(ficnames):
+def ffn_link_finder(fic_names):
     links_found = []
-    for ficname in ficnames:
+    for fic_name in fic_names:
         time.sleep(randint(1, 3))
-        search_request = 'site:fanfiction.net/s/ ' + ficname
+
+        search_request = 'site:fanfiction.net/s/ ' + fic_name
         print("SEARCHING: ", search_request)
+
         search_results = search(search_request, num=1, stop=1)
         links_found.append(next(search_results))
+
     return links_found
 
 
-def ffn_commentmaker(links):
+def ffn_comment_maker(links):
     comment = ''
     for link in links:
-        comment += ffn_descriptionmaker(link)
-        comment += '\n&nbsp;\n'
-        comment += '\n'
+        comment = '{0}\n&nbsp;\n\n'.format(ffn_description_maker(link))
     return comment
 
 
-def ffn_descriptionmaker(link):
+def ffn_description_maker(link):
     current = Story(link)
-    decodedTitle = current.title.decode('ascii', errors='replace')
-    decodedAuthor = current.author.decode('ascii', errors='replace')
-    decodedSummary = current.summary.decode('ascii', errors='replace')
-    decodedData = current.data.decode('ascii', errors='replace')
-    ws = '\n\n '
 
-    header =   '[***{0}***]({1}) '.format(decodedTitle, link)   #'[***' +  decodedTitle + '***]' + '(' + link + ')' + ' '
-    header += ' by [*{0}*]({1})'.format(decodedAuthor, current.authorlink) #'[*' + decodedAuthor + '*]' + '(' + current.authorlink + ')'
-    summary = '> ' + decodedSummary
-    data = '> ' + decodedData
-    return '{0} {1} {2} {1} {3} {1}'.format(header, ws, summary, data)
+    decoded_title = current.title.decode('ascii', errors='replace')
+    decoded_author = current.author.decode('ascii', errors='replace')
+    decoded_summary = current.summary.decode('ascii', errors='replace')
+    decoded_data = current.data.decode('ascii', errors='replace')
+
+    #more pythonic string formatting
+    header =   '[***{0}***]({1}) by [*{2}*]({3})'.format(decoded_title, link, decoded_author, current.authorlink)
+     #'[***' +  decodedTitle + '***]' + '(' + link + ')' + ' '
+    #'[*' + decodedAuthor + '*]' + '(' + current.authorlink + ')'
+
+    return '{0}\n\n>{1}\n\n>{1}\n\n'.format(header, decoded_summary, decoded_data)
 
 main()
