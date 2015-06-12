766177b55c1284d0203190b31a846e57e978c1a5
Tushar Singal <tusharsingal@gmail.com>
2015-06-11 22:21:31 -0400

    Rewrote most backend to be more modular

--- fanfiction_backend.py
+++ fanfiction_backend.py
@@ -1,7 +1,70 @@
-from lxml import html
-from lxml import etree
+import praw
+import time
+import pickle
+import re
+import os
+import sys
+import argparse
 import requests
 import urllib.request
+from random import randint
+from pprint import pprint
+from google import search
+from lxml import html
+from lxml import etree
+
+
+def ffn_make_from_requests(requests):
+    links = ffn_link_finder(requests)
+    found_ffn = ffn_comment_maker(links)
+    return found_ffn
+
+
+def ffn_link_finder(fic_names):
+    links_found = []
+    for fic_name in fic_names:
+
+        # Obfuscation.
+        time.sleep(randint(1, 3))
+        sleep_milliseconds = randint(500, 3000)
+        time.sleep(sleep_milliseconds / 1000)
+
+        search_request = 'site:fanfiction.net/s/ ' + fic_name
+        print("SEARCHING: ", search_request)
+
+        search_results = search(search_request, num=1, stop=1)
+        link_found = next(search_results)
+        links_found.append(link_found)
+        print("FOUND: " + link_found)
+
+    return links_found
+
+
+def ffn_comment_maker(links):
+    comment = ''
+    for link in links:
+        comment += '{0}\n&nbsp;\n\n'.format(ffn_description_maker(link))
+    return comment
+
+
+def ffn_description_maker(link):
+    current = Story(link)
+    decoded_title = current.title.decode('ascii', errors='replace')
+    decoded_author = current.author.decode('ascii', errors='replace')
+    decoded_summary = current.summary.decode('ascii', errors='replace')
+    decoded_data = current.data.decode('ascii', errors='replace')
+
+    print("Making a description for " + decoded_title)
+
+    # More pythonic string formatting.
+    header = '[***{0}***]({1}) by [*{2}*]({3})'.format(decoded_title,
+                                                       link, decoded_author, current.authorlink)
+
+    formatted_description = '{0}\n\n>{1}\n\n>{2}\n\n'.format(
+        header, decoded_summary, decoded_data)
+    # print("Description for " + decoded_title + ": \n" + formatted_description)
+    print(formatted_description)
+    return formatted_description
 
 
 class Story:
--- fanfiction_bot.py
+++ fanfiction_bot.py
@@ -4,26 +4,133 @@ import pickle
 import re
 import os
 import sys
+import argparse
+import fanfiction_backend
 from fanfiction_backend import Story
 from random import randint
 from pprint import pprint
 from google import search
-import argparse
 
 
 USER_AGENT = "Python:FanfictionComment:v0.001a (by /u/tusing)"
 r = praw.Reddit(USER_AGENT)
-SUBREDDIT = None
+DEFAULT_SUBREDDITS = ['HPFanfiction']
+SUBREDDIT_LIST = []
 CHECKED_COMMENTS = []
 
 
+REGEXPS = {'linkffn\((.*?)\)': 'ffn'}
+FOOTER = "\n*Read usage tips and tricks  [here](https://github.com/tusing/reddit-ffn-bot/blob/master/README.md). Brought to you by me - /u/tusing's bot, with improvements by /u/MikroMan.*"
+
+
 def main():
-    # CHECKED_COMMENTS = pickle.load(replies)
     login_to_reddit()
+    load_checked_comments()
+    load_subreddits()
 
     while True:
-        parse_submissions()
-        pause(10, 0)
+        for SUBREDDIT in SUBREDDIT_LIST:
+            parse_submissions(r.get_subreddit(SUBREDDIT))
+            pause(1, 0)
+
+
+def parse_arguments():
+    # initialize parser and add options for username and password
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', help='define Reddit login username')
+    parser.add_argument('-p', '--password', help='define Reddit login password')
+    parser.add_argument(
+        '-s', '--subreddit', help='define target subreddit; can be used with -a')
+    parser.add_argument('-a', '--all', help='run on all default subreddits')
+    args = parser.parse_args()
+    return args.user, args.password, args.subreddit, args.all
+
+
+def login_to_reddit():
+    user_name, user_pw, user_subreddit, all_subreddits = parse_arguments()
+    print("Logging in...")
+    r.login(user_name, user_pw)
+    print("Logged in.")
+
+
+def load_subreddits():
+    global SUBREDDIT_LIST
+    user_name, user_pw, user_subreddit, all_subreddits = parse_arguments()
+    print("Loading subreddits...")
+    if all_subreddits is True:
+        SUBREDDIT_LIST.append(DEFAULT_SUBREDDITS)
+    if user_subreddit is True:
+        SUBREDDIT_LIST.append(user_subreddit)
+    if len(SUBREDDIT_LIST) == 0:
+        SUBREDDIT_LIST.append('tusingtestfield')
+    print("LOADED SUBREDDITS: ", SUBREDDIT_LIST)
+
+
+def check_comment(id):
+    global CHECKED_COMMENTS
+    CHECKED_COMMENTS.append(str(id))
+    with open('CHECKED_COMMENTS.txt', 'w') as file:
+        for id in CHECKED_COMMENTS:
+            file.write(str(id) + '\n')
+
+
+def load_checked_comments():
+    global CHECKED_COMMENTS
+    print('Loading CHECKED_COMMENTS...')
+    with open('CHECKED_COMMENTS.txt', 'r') as file:
+        CHECKED_COMMENTS = [str(line.rstrip('\n')) for line in file]
+    print('Loaded CHECKED_COMMENTS. Contains:')
+    print(CHECKED_COMMENTS)
+
+
+def parse_submissions(SUBREDDIT):
+    print("PARSING SUBMISSIONS ON SUBREDDIT ", SUBREDDIT)
+    for submission in SUBREDDIT.get_hot(limit=10):
+        print("Checking SUBMISSION: ", submission.id)
+        flat_comments = praw.helpers.flatten_tree(submission.comments)
+        for comment in flat_comments:
+            print('Checking COMMENT: ' + comment.id + ' in submission ' + submission.id)
+            if str(comment.id) in CHECKED_COMMENTS:
+                print("Comment " + comment.id + " already parsed!")
+            else:
+                print("Parsing comment ", comment.id)
+                make_reply(comment, comment.id)
+
+
+def make_reply(comment, id):
+
+    reply = formulate_reply(comment.body)
+
+    if reply is None:
+        check_comment(comment.id)
+        print("Empty reply!")
+    elif len(reply) > 10:
+        print('Outgoing reply to ' + id + ':\n' + reply + FOOTER)
+        comment.reply(reply + FOOTER)
+        check_comment(comment.id)
+        pause(2, 5)
+    else:
+        print("No reply conditions met.")
+        check_comment(comment.id)
+
+
+def formulate_reply(comment_body):
+    requests = {}
+    for expr in REGEXPS.keys():
+        tofind = re.findall(expr, comment_body)
+        requests[REGEXPS[expr]] = tofind
+    print("FINDING: ", requests)
+    return parse_comment_requests(requests)
+
+
+def parse_comment_requests(requests):
+    comments_from_sources = []
+    ffn_requests = requests['ffn']
+    print("FFN requests: ", ffn_requests)
+    ffn_comment = fanfiction_backend.ffn_make_from_requests(ffn_requests)
+    dlp_comment = ""
+    ao3_comment = ""
+    return ffn_comment + dlp_comment + ao3_comment
 
 
 def pause(minutes, seconds):
@@ -43,129 +150,5 @@ def pause(minutes, seconds):
         sys.stdout.write("\rCountdown bypassed!            \n")
         pass
 
-
-def parse_arguments():
-    # initialize parser and add options for username and password
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--user', help='define Reddit login username')
-    parser.add_argument('-p', '--password', help='define Reddit login password')
-    parser.add_argument('-s', '--subreddit', help='define target subreddit')
-
-    args = parser.parse_args()
-
-    return args.user, args.password, args.subreddit
-
-
-def login_to_reddit():
-    global SUBREDDIT
-    user_name, user_pw, user_subreddit = parse_arguments()
-
-    print("Logging in...")
-    r.login(user_name, user_pw)
-    print("Logged in.")
-
-    print("Loading subreddit...")
-    if user_subreddit is False:
-        user_subreddit = 'tusingtestfield'
-        SUBREDDIT = r.get_subreddit(user_subreddit)
-    else:
-        SUBREDDIT = r.get_subreddit(user_subreddit)
-    print("Loaded subreddit " + user_subreddit)
-
-    print('Loading CHECKED_COMMENTS...')
-    global CHECKED_COMMENTS
-    with open('CHECKED_COMMENTS.txt', 'r') as file:
-        CHECKED_COMMENTS = [str(line.rstrip('\n')) for line in file]
-    print('Loaded CHECKED_COMMENTS.')
-
-
-def mark_as_done(id):
-    global CHECKED_COMMENTS
-    CHECKED_COMMENTS.append(str(id))
-
-    with open('CHECKED_COMMENTS.txt', 'w') as file:
-        for id in CHECKED_COMMENTS:
-            file.write(str(id) + '\n')
-
-
-def parse_submissions():
-    print('CHECKED_COMMENTS contains:')
-    print(CHECKED_COMMENTS)
-
-    for submission in SUBREDDIT.get_hot(limit=10):
-        print("Checking SUBMISSION: ", submission.id)
-        flat_comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in flat_comments:
-            print('Checking COMMENT: ' + comment.id + ' in submission ' + submission.id)
-            if str(comment.id) in CHECKED_COMMENTS:
-                print("Comment " + comment.id + " already parsed!")
-            else:
-                print("Parsing comment ", comment.id)
-                make_comment(comment, comment.id)
-
-
-def make_comment(comment, id):
-    footer = "\n*Read usage tips and tricks  [here](https://github.com/tusing/reddit-ffn-bot/blob/master/README.md). Brought to you by me - /u/tusing's bot, with improvements by /u/MikroMan.*"
-    REGEXPS = {'linkffn\((.*?)\)': 'ffn'}
-    requested = {}
-
-    contents = comment.body
-    for expr in REGEXPS.keys():
-        tofind = re.findall(expr, contents)
-        requested[REGEXPS[expr]] = tofind
-    print("FINDING: ", requested)
-    links = ffn_link_finder(requested['ffn'])
-    found_ffn = ffn_comment_maker(links)
-    if len(found_ffn) > 10:
-        print('Outgoing reply to ' + id + ':\n' + found_ffn + footer)
-        comment.reply(found_ffn + footer)
-        mark_as_done(comment.id)
-        pause(2, 5)  # pause for 9 minutes and 5 seconds before new comment
-
-
-def ffn_link_finder(fic_names):
-    links_found = []
-    for fic_name in fic_names:
-
-        # Obfuscation.
-        time.sleep(randint(1, 3))
-        sleep_milliseconds = randint(500, 3000)
-        time.sleep(sleep_milliseconds / 1000)
-
-        search_request = 'site:fanfiction.net/s/ ' + fic_name
-        print("SEARCHING: ", search_request)
-
-        search_results = search(search_request, num=1, stop=1)
-        link_found = next(search_results)
-        links_found.append(link_found)
-        print("FOUND: " + link_found)
-
-    return links_found
-
-
-def ffn_comment_maker(links):
-    comment = ''
-    for link in links:
-        comment += '{0}\n&nbsp;\n\n'.format(ffn_description_maker(link))
-    return comment
-
-
-def ffn_description_maker(link):
-    current = Story(link)
-    decoded_title = current.title.decode('ascii', errors='replace')
-    decoded_author = current.author.decode('ascii', errors='replace')
-    decoded_summary = current.summary.decode('ascii', errors='replace')
-    decoded_data = current.data.decode('ascii', errors='replace')
-
-    print("Making a description for " + decoded_title)
-
-    # More pythonic string formatting.
-    header = '[***{0}***]({1}) by [*{2}*]({3})'.format(decoded_title,
-                                                       link, decoded_author, current.authorlink)
-
-    formatted_description = '{0}\n\n>{1}\n\n>{2}\n\n'.format(
-        header, decoded_summary, decoded_data)
-    # print("Description for " + decoded_title + ": \n" + formatted_description)
-    return formatted_description
 
 main()
