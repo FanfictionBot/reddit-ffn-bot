50ad435ed4f9d57c6457d4e2cf78819196f649c3
StuxCrystal <stuxcrystal@gmail.com>
2015-06-14 16:09:19 +0200

    Added extended cache implementation.

---
+++ ffn_bot/cache.py
@@ -0,0 +1,71 @@
+import time
+from google import search
+from requests import get
+from collections import OrderedDict
+
+
+class LimitedSizeDict(OrderedDict):
+    """The actual cache implementation."""
+    
+    def __init__(self, *args, **kwds):
+        self.size_limit = kwds.pop("size_limit", None)
+        OrderedDict.__init__(self, *args, **kwds)
+        self._check_size_limit()
+        
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._check_size_limit()
+        
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                self.popitem(last=False)
+
+
+class RequestCache(object):
+    """
+    Cache for search requests and page-loads.
+    """
+    
+    # Marker for non cached objects.
+    EMPTY_RESULT = []
+    
+    def __init__(self, max_size=10000, expire_time=30*60*1000):
+        self.cache = LimitedSizeDict(max_size)
+        self.expire_time = expire_time
+        
+    def hit_cache(self, type, query):
+        """Check if the value is in the cache."""
+        
+        result = self.cache.get("%s:%s"%(type, query), self.EMPTY_RESULT)
+        if result is not self.EMPTY_RESULT:
+            # Let values expire.
+            if time.time() - result[1] <= self.expire_time:
+                self.push_cache(type, query, result[0])
+                return result[0]
+        raise KeyError("Not cached")
+        
+    def push_cache(self, type, query, data):
+        """Push a value into the cache."""
+        self.cache["%s:%s"%(type, query)] = (data, time.time())
+        
+    def get_page(self, page):
+        try:
+            return self.hit_cache("get", page)
+        except KeyError:
+            pass
+            
+        result = get(page).text
+        self.push_cache("get", page, result)
+        return result
+        
+    def search(self, query):
+        try:
+            return self.hit_cache("search", query)
+        except KeyError:
+            pass
+        
+        result = next(search(query, num=1, stop=1), None)
+        self.push_cache("search", query, result)
+        return result
+        
