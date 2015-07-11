a2cb713836e8b9d54f162a4be546a38ac3a43ca6
Roland Netzsch <stux@yorunohikari.moe>
2015-07-11 15:30:11 +0200

    Autoimport fetchers

--- ffn_bot/fetchers/__init__.py
+++ ffn_bot/fetchers/__init__.py
@@ -1,14 +1,43 @@
-from ffn_bot.fetchers import aff
-from ffn_bot.fetchers import ao3
-from ffn_bot.fetchers import ffn
-from ffn_bot.fetchers import ffa
+import os.path
+from itertools import chain
+from pkgutil import iter_modules
 
-SITES = [
-    aff.AdultFanfiction(),
-    ao3.ArchiveOfOurOwn(),
-    ffn.FictionPressSite(),
-    ffn.FanfictionNetSite(),
-    ffa.HPFanfictionArchive(),
-]
+from ffn_bot.site import Site
+
+
+# This bit of complexity allows the
+# Module to automatically discover its own fetchers
+# Just drop the python file in the directory and it will be loaded.
+def _try_caller(func):
+    try:
+        return func()
+    except Exception:
+        return None
+
+
+SITES = list(filter(
+    lambda x: x is not None,
+    chain.from_iterable(
+        (
+            (
+                # Find and instantiate all Sites
+                _try_caller(getattr(module, name))
+                for name in dir(module)
+                if isinstance(getattr(module, name), type) and
+                    issubclass(
+                        getattr(module, name),
+                        Site
+                    )
+            )
+            # Import Submodules
+            for module in (
+                loader.find_module(module).load_module(module)
+                for loader, module, ispkg in iter_modules(
+                    [os.path.dirname(__file__)]
+                )
+            )
+        )
+    )
+))
 
 __all__ = ["SITES"]
