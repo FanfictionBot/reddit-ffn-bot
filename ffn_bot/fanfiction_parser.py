7454ea9be8448bf88f8718f490bc991a998d9a03
Roland Netzsch <stux@yorunohikari.moe>
2015-07-11 10:40:39 +0200

    Switched FFN to metaparse

--- ffn_bot/fanfiction_parser.py
+++ ffn_bot/fanfiction_parser.py
@@ -1,16 +1,10 @@
 import re
-import sys
-import time
-import logging
-import requests
-import itertools
-
 from ffn_bot import bot_tools
 from ffn_bot import site
 from ffn_bot.cache import default_cache
+from ffn_bot.metaparse import Metaparser, parser
 
 from random import randint
-from google import search
 from lxml import html
 
 __all__ = ["FanfictionNetSite", "FictionPressSite"]
@@ -18,16 +12,86 @@ __all__ = ["FanfictionNetSite", "FictionPressSite"]
 LINK_REGEX = "http(s?)://((www|m)\\.)?%s/s/(?P<sid>\\d+).*"
 ID_LINK = "https://www.{0}/s/%s"
 
+FFN_GENRES = [
+    "Adventure", "Angst", "Crime", "Drama", "Family", "Fantasy", "Friendship",
+    "General", "Horror", "Humor", "Hurt-Comfort", "Mystery", "Parody",
+    "Poetry", "Romance", "Sci-Fi", "Spiritual", "Supernatural", "Suspense",
+    "Tragedy", "Western"
+]
+
+
+class FanfictionParser(Metaparser):
+    CATEGORY_TYPE = "Category"
+
+    @staticmethod
+    def get_story_information(tree):
+        if tree.xpath('//*[@id="profile_top"]/span[1]/img'):
+            return "".join(tree.xpath('//*[@id="profile_top"]/span[4]//text()'))
+        else:
+            return "".join(tree.xpath('//*[@id="profile_top"]/span[3]//text()'))
+    @parser
+    @classmethod
+    def parse_category(cls, id, tree):
+        return (
+            cls.CATEGORY_TYPE,
+            tree.xpath('//*[@id="pre_story_links"]/span/a[last()]/text()')[0]
+        )
+
+    @parser
+    @classmethod
+    def parse_metadata_simple(cls, id, tree):
+        story_info = cls.get_story_information(tree)
+        print(story_info)
+        for part in re.split(r"\s+-\s+", story_info):
+            subparts = re.split(r":\s+", part)
+            if len(subparts) == 2:
+                yield subparts
+
+    @parser
+    @classmethod
+    def parse_unnamed_parts(cls, id, tree):
+        n_unnamed = 0
+        story_info = cls.get_story_information(tree)
+        for part in re.split(r"\s+-\s+", story_info):
+            subparts = re.split(r":\s+", part)
+            if len(subparts) == 2:
+                continue
+
+            subpart = subparts[0]
+            if n_unnamed == 0:
+                yield "Language", subpart
+            elif (
+                    n_unnamed == 1
+                    and sum(
+                        (g.strip() in FFN_GENRES)
+                        for g in subpart.split("/")
+                    )
+            ):
+                yield "Genre", subpart
+            else:
+                yield "Characters", subpart
+            n_unnamed += 1
+
+    @classmethod
+    def create_implementation(cls, category):
+        return type(cls).__new__(
+            type(cls),
+            "<FFNParser>",
+            (cls,),
+            {"CATEGORY_TYPE": category}
+        )
+
 
 class FanfictionBaseSite(site.Site):
     # All regexps are automatically case insensitive for sites.
 
-    def __init__(self, site, command, name=None):
+    def __init__(self, site, command, name=None, category="Category"):
         super(FanfictionBaseSite, self).__init__(command + r"\((.*?)\)", name)
         self.site = site
         self.link_regex = re.compile(
             LINK_REGEX % self.site.replace(".", "\\."), re.IGNORECASE)
         self.id_link = ID_LINK.format(self.site)
+        self.parser = FanfictionParser.create_implementation(category)
 
     def from_requests(self, requests, context):
         # I'd love to use 'yield from'
@@ -37,7 +101,7 @@ class FanfictionBaseSite(site.Site):
     def process(self, request, context):
         try:
             link = self.find_link(request, context)
-        except (StopIteration, Exception) as e:
+        except (StopIteration, Exception):
             bot_tools.print_exception()
             return None
 
@@ -46,15 +110,15 @@ class FanfictionBaseSite(site.Site):
 
         try:
             return self.generate_response(link, context)
-        except Exception as e:
+        except Exception:
             bot_tools.print_exception()
             return None
 
     def generate_response(self, link, context):
-        return Story(link, self.site, context)
+        return Story(link, self.site, context, self.parser)
 
     def find_link(self, fic_name, context):
-        # Prevent users from crashing program with bad link names.
+        # Prevent users from crashing program eith bad link names.
         fic_name = fic_name.encode('ascii', errors='replace')
         fic_name = fic_name.decode('ascii', errors='replace')
 
@@ -76,13 +140,14 @@ class FanfictionBaseSite(site.Site):
             self.generate_response(self.id_link % id, context)
             for _,_,_,id in self.link_regex.findall(body)
         )
+
+
 class Story(site.Story):
 
-    def __init__(self, url, site, context):
+    def __init__(self, url, site, context, parser):
         super(Story, self).__init__(context)
         self.url = url
         self.site = site
-        self.raw_stats = []
         self.stats = ""
 
         self.title = ""
@@ -90,9 +155,8 @@ class Story(site.Story):
         self.authorlink = ""
         self.summary = ""
 
+        self.parser = parser
         self.parse_html()
-        self.encode()
-        self.decode()
 
     def get_url(self):
         return "http://www.%s/s/%s/1/" % (
@@ -101,7 +165,10 @@ class Story(site.Story):
         )
 
     def parse_html(self):
-        page = default_cache.get_page(self.get_url(), throttle=randint(1000,4000)/1000)
+        page = default_cache.get_page(
+            self.get_url(),
+            throttle=randint(1000,4000)/1000
+        )
         tree = html.fromstring(page)
 
         self.title = (tree.xpath('//*[@id="profile_top"]/b/text()'))[0]
@@ -110,43 +177,24 @@ class Story(site.Story):
         self.authorlink = 'https://www.' + self.site + \
             tree.xpath('//*[@id="profile_top"]/a[1]/@href')[0]
         self.image = tree.xpath('//*[@id="profile_top"]/span[1]/img')
-
-        self.raw_stats = []
-        self.raw_stats.extend(
-            tree.xpath('//*[@id="pre_story_links"]/span/a[last()]/text()'))
-        self.raw_stats.extend(['\n'])
-
-        # XPath changes depending on the presence of an image
-        if len(self.image) is not 0:
-            self.raw_stats.extend(tree.xpath('//*[@id="profile_top"]/span[4]//text()'))
-        else:
-            self.raw_stats.extend(tree.xpath('//*[@id="profile_top"]/span[3]//text()'))
-
-    def encode(self):
-        self.title = self.title.encode('ascii', errors='replace')
-        self.author = self.author.encode('ascii', errors='replace')
-        self.summary = self.summary.encode('ascii', errors='replace')
-        self.stats = "".join(itertools.chain(
-            (self.stats,), self.raw_stats)).encode('ascii', errors='replace')
-
-    def decode(self):
-        _decode = lambda s: s.decode('ascii', errors='replace')
-        self.title = _decode(self.title)
-        self.author = _decode(self.author)
-        self.summary = _decode(self.summary)
-        self.stats = _decode(self.stats)
+        self.tree = tree
+        self.stats = self.parser.parse_to_string(None, tree)
 
 
 class FanfictionNetSite(FanfictionBaseSite):
 
     def __init__(self, command="linkffn", name=None):
-        super(FanfictionNetSite, self).__init__("fanfiction.net", "linkffn", name)
+        super(FanfictionNetSite, self).__init__(
+            "fanfiction.net", "linkffn", name, "Fandom"
+        )
 
 
 class FictionPressSite(FanfictionBaseSite):
 
     def __init__(self, command="linkfp", name=None):
-        super(FictionPressSite, self).__init__("fictionpress.com", "linkfp", name)
+        super(FictionPressSite, self).__init__(
+            "fictionpress.com", "linkfp", name, "Category"
+        )
 
 # We don't need to cache the story objects anymore.
 
