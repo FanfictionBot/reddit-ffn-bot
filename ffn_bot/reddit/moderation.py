ecfe66ff48f4964b461bc5c56cf81ec60748422e
Tushar Singal <tusharsingal@gmail.com>
2016-01-10 20:52:17 -0500

    Fixing !refresh.

--- ffn_bot/reddit/moderation.py
+++ ffn_bot/reddit/moderation.py
@@ -168,7 +168,7 @@ class ModerativeCommands(object, metaclass=ModerativeCommandsMeta):
 
         # Since parent redirects to this method now, modify the force marker
         # before doing anything.
-        parent_markers = set()
+        parent_markers = {'force'}
         parent_markers |= additional
         self.handle(comment_with_requests, parent_markers)
 
