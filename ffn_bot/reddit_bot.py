f29a336ca65118998ca0bbd7ccb894179b30003a
Tushar Singal <tusharsingal@gmail.com>
2016-01-14 14:00:17 -0500

    Updated footer more.

--- ffn_bot/reddit_bot.py
+++ ffn_bot/reddit_bot.py
@@ -22,11 +22,12 @@ DEFAULT_SUBREDDITS = ['HPFanfiction']
 SUBREDDIT_LIST = set()
 CHECKED_COMMENTS = None
 FOOTER = "\n".join([
-    r"**FanfictionBot**^(1.3.0) **|** \[[Usage][1]\] | \[[Changelog][2]\] | \[[Issues][3]\] | \[[GitHub][4]\]",
+    r"**FanfictionBot**^(1.3.0) **|** \[[Usage][1]\] | \[[Changelog][2]\] | \[[Issues][3]\] | \[[GitHub][4]\] | \[[Contact][5]\]",
     r'[1]: https://github.com/tusing/reddit-ffn-bot/wiki/Usage       "How to use the bot"',
     r'[2]: https://github.com/tusing/reddit-ffn-bot/wiki/Changelog   "What changed until now"',
     r'[3]: https://github.com/tusing/reddit-ffn-bot/issues/          "Bugs? Suggestions? Enter them here!"',
-    r'[4]: https://github.com/tusing/reddit-ffn-bot/                 "Fork me on GitHub"'
+    r'[4]: https://github.com/tusing/reddit-ffn-bot/                 "Fork me on GitHub"',
+    r'[5]: https://www.reddit.com/message/compose?to=%2Fu%2Ftusing   "The maintainer"'
 ])
 FOOTER += "\n\n^^^^^^^^^^^^^^^^^ffnbot!ignore"
 
