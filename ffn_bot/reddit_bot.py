f5909efcdf090663ae21f9274bc5d0e671699e4f
Tushar Singal <tusharsingal@gmail.com>
2016-01-20 21:45:52 -0800

    Updated version number.

--- ffn_bot/reddit_bot.py
+++ ffn_bot/reddit_bot.py
@@ -22,7 +22,7 @@ DEFAULT_SUBREDDITS = ['HPFanfiction']
 SUBREDDIT_LIST = set()
 CHECKED_COMMENTS = None
 FOOTER = "\n".join([
-    r"**FanfictionBot**^(1.3.0) **|** \[[Usage][1]\] | \[[Changelog][2]\] | \[[Issues][3]\] | \[[GitHub][4]\] | \[[Contact][5]\]",
+    r"**FanfictionBot**^(1.3.5) **|** \[[Usage][1]\] | \[[Changelog][2]\] | \[[Issues][3]\] | \[[GitHub][4]\] | \[[Contact][5]\]",
     r'[1]: https://github.com/tusing/reddit-ffn-bot/wiki/Usage       "How to use the bot"',
     r'[2]: https://github.com/tusing/reddit-ffn-bot/wiki/Changelog   "What changed until now"',
     r'[3]: https://github.com/tusing/reddit-ffn-bot/issues/          "Bugs? Suggestions? Enter them here!"',
