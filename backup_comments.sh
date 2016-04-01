188b4fb93499bb1abd912eaf969de1be5483ac3c
Tushar Singal <tusharsingal@gmail.com>
2016-04-01 02:07:16 -0700

    Fix newline issue

--- backup_comments.sh
+++ backup_comments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This program requires gist: https://github.com/defunkt/gist
+# This script requires gist: https://github.com/defunkt/gist
 
 usage() { echo "Usage: $0 [-i <comment file in>] [-o <log file out>] [-s <seconds to sleep>]" 1>&2; exit 1; }
 comments_file=CHECKED_COMMENTS.txt
@@ -31,7 +31,7 @@ echo "Using sleep time: " $sleep_time
 while true
 do
   echo "$(date): Backing up..."
-  echo -n $(date) - $(gist $comments_file) >> $backup_log
+  echo $(date) - $(gist $comments_file)\ >> $backup_log
   echo "$(date): Sleeping for " $sleep_time "seconds..."
   sleep $sleep_time
 done
