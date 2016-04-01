758a20e068595b3595ef2dd6e48aa6b09ff6940b
Tushar Singal <tusharsingal@gmail.com>
2016-04-01 01:56:57 -0700

    better argument handling

--- backup_comments.sh
+++ backup_comments.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 # This program requires gist: https://github.com/defunkt/gist
-# Usage: backup_comments.sh [comments file] [file for storing URL] [wait time in between backups]
 
+usage() { echo "Usage: $0 [-i <comment file in>] [-o <log file out>] [-s <seconds to sleep>]" 1>&2; exit 1; }
 comments_file=CHECKED_COMMENTS.txt
 backup_log=BACKUP_LOCATIONS.txt
 sleep_time=3600
 
-if [ -z "$1" ]
-  then
-    comments_file=$1
-    echo "Using specified comments file: " $comments_file
-  else
-    echo "Using default comments file: " $comments_file
-fi
+while getopts ":i:o:s:" o; do
+    case "${o}" in
+        i)
+            comments_file=${OPTARG}
+            ;;
+        o)
+            backup_log=${OPTARG}
+            ;;
+        s)
+            sleep_time=${OPTARG}
+            ;;        
+        *)
+            usage
+            ;;
+    esac
+done
 
-if [ -z "$2" ]
-  then
-    backup_log=$2
-    echo "Storing backup locations in specified file: " $backup_log
-  else
-    echo "Storing backup locations in default file: " $backup_log
-fi
-
-if [ -z "$3" ]
-  then
-    sleep_time=$3
-    echo "Using specified sleep time: " $sleep_time
-  else
-    echo "Using default sleep time: " $sleep_time
-fi
+echo "Using comments file: " $comments_file
+echo "Storing backup locations in file: " $backup_log
+echo "Using sleep time: " $sleep_time
 
   
 while true
