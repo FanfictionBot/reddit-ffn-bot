cf94951c159000453ba33980a07a75183868a7b5
Tushar Singal <tusharsingal@gmail.com>
2016-04-01 02:27:16 -0700

    allow login and use of existing gist

--- backup_comments.sh
+++ backup_comments.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This script requires gist: https://github.com/defunkt/gist
 
-usage() { echo "Usage: $0 [-i <comment file in>] [-o <log file out>] [-s <seconds to sleep>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-i <comment file in>] [-o <log file out>] [-s <seconds to sleep>] [-a <Login to GitHub? TRUE|FALSE>] [-e <existing Gist id (requires login)>]" 1>&2; exit 1; }
 comments_file=CHECKED_COMMENTS.txt
 backup_log=BACKUP_LOCATIONS.txt
 sleep_time=3600
+existing_gist=""
 
 while getopts ":i:o:s:" o; do
     case "${o}" in
@@ -16,7 +17,11 @@ while getopts ":i:o:s:" o; do
             ;;
         s)
             sleep_time=${OPTARG}
-            ;;        
+            ;;     
+        a)
+            gist --login
+        e)
+            existing_gist=${OPTARG}
         *)
             usage
             ;;
@@ -31,7 +36,13 @@ echo "Using sleep time: " $sleep_time
 while true
 do
   echo "$(date): Backing up..."
-  echo $(date) - $(gist $comments_file)\ >> $backup_log
+  if [ "$existing_gist" != "" ]
+    then
+      echo "Using existing gist ID: $existing_gist"
+      echo $(date) - $(gist -p -u $existing_gist -d "FanfictionBot Comment Backup: $date" $comments_file)\ >> $backup_log
+    else
+      echo $(date) - $(gist -d "FanfictionBot Comment Backup: $date" $comments_file)\ >> $backup_log
+  fi
   echo "$(date): Sleeping for " $sleep_time "seconds..."
   sleep $sleep_time
 done
